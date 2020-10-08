9f30e99abbf2b6ef3b887d1748adb083606cf3ae
Ahsan Shahid <ahsanshahidcool@gmail.com>
2020-10-07 22:25:53 -0400

    Initial signup form added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './Signup.css';
 
-class Signup extends Component {
-  render() {
-    return <h1>Signup Page</h1>;
-  }
+export default function SignUp() {
+  return (
+    <div className="form-container">
+      <h1>Sign Up Form</h1>
+      <form className="form" noValidate autoComplete="off">
+        <TextField id="userName" label="User Name" variant="outlined" fullWidth />
+        <TextField id="email" label="Email" variant="outlined" fullWidth />
+        <TextField id="password" label="Password" variant="outlined" fullWidth />
+        <TextField id="confirmPassword" label="Confirm Password" variant="outlined" fullWidth />
+      </form>
+      <Button variant="contained" color="primary">
+        Sign Up
+      </Button>
+    </div>
+  );
 }
-
-export default Signup;
