4d09bef9e500f131c3ddd72cc6450c73ba5a5465
Ahsan Shahid <ahsanshahidcool@gmail.com>
2020-10-08 00:20:45 -0400

    Initial structure all set up

---
+++ src/API/SignUp/SignUp.js
@@ -0,0 +1,34 @@
+// This module creates separation between the API and our components
+// If the api changes, we will need to update this module only
+// Rest of the components using the API won't need modifications
+// This module can also be unit tested and expanded easily for new functionality
+// without affecting rest of the application
+
+// Duties of this module
+// 1. Build a query based on user input
+// 2. Issue the query
+// 3. Normalize the results for consumption by other components
+
+function buildSignUpQuery({
+  userName, email, password, confirmPassword,
+}) {
+  return {
+    method: 'POST',
+    body: {
+      userName,
+      email,
+      password,
+      confirmPassword,
+    },
+  };
+}
+
+function normalizeResponse(response) {
+  return response;
+}
+
+
+export default SignUp(payload) {
+    const query = buildSignUpQuery(payload);
+
+}
